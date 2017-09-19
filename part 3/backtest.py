6b067d970b44598a50f8b2ac3487e882ba590799
Deadlift <Deadlift@192.168.0.149>
2017-09-19 17:56:04 +0200

    I believe in miracles

--- part 3/backtest.py
+++ part 3/backtest.py
@@ -1,4 +1,6 @@
 import sys, getopt
+import datetime
+from openpyxl import Workbook
 
 from botchart import BotChart
 from botstrategy import BotStrategy
@@ -7,9 +9,25 @@ def main(argv):
 	chart = BotChart("poloniex","BTC_XMR",300)
 
 	strategy = BotStrategy()
+	print strategy.spreadsheet
 
 	for candlestick in chart.getPoints():
 		strategy.tick(candlestick)
 
+	#Create Worbook with an active sheet
+	m_wb = Workbook()
+	wslist = m_wb.active
+	wslist.title = 'Charts'
+	
+	list = strategy.spreadsheet
+	
+	for row_list in list:
+	    wslist.append(row_list)
+	
+	# Save
+	dt = str(datetime.datetime.now())
+	wb_name = 'CryptoChartInfo' + dt[0:10] + '.xlsx'
+	m_wb.save(wb_name)
+	
 if __name__ == "__main__":
 	main(sys.argv[1:])
--- part 3/botstrategy.py
+++ part 3/botstrategy.py
@@ -3,6 +3,7 @@ from botindicators import BotIndicators
 from bottrade import BotTrade
 
 class BotStrategy(object):
+	
 	def __init__(self):
 		self.output = BotLog()
 		self.prices = []
@@ -12,6 +13,7 @@ class BotStrategy(object):
 		self.currentClose = ""
 		self.numSimulTrades = 1
 		self.indicators = BotIndicators()
+		self.spreadsheet = []
 
 	def tick(self,candlestick):
 		self.currentPrice = float(candlestick['weightedAverage'])
@@ -25,6 +27,8 @@ class BotStrategy(object):
 		self.evaluatePositions()
 		self.updateOpenTrades()
 		self.showPositions()
+		mess = str(candlestick['weightedAverage'])
+		self.GenSpreadsheetInfo(message="Price: "+str(candlestick['weightedAverage'])+"\tMoving Average: "+str(self.indicators.movingAverage(self.prices,15)))
 
 	def evaluatePositions(self):
 		openTrades = []
@@ -33,7 +37,7 @@ class BotStrategy(object):
 				openTrades.append(trade)
 
 		if (len(openTrades) < self.numSimulTrades):
-			if (self.currentPrice < self.indicators.movingAverage(self.prices,15)):
+			if (self.currentPrice < self.indicators.movingAverage(self.prices,20)):
 				self.trades.append(BotTrade(self.currentPrice,stopLoss=.0001))
 
 		for trade in openTrades:
@@ -48,5 +52,18 @@ class BotStrategy(object):
 	def showPositions(self):
 		for trade in self.trades:
 			trade.showTrade()
+			
+	def plotData(self):
+		print 'Plot Now'
+	
+	def GenSpreadsheetInfo(self, message):
+		newList = []
+		strMes = str(message.encode('utf8'))
+		newList.append(strMes)
+		self.spreadsheet.append(newList)
+		
+	def GetSpreadsheetInfo(self,message):
+		return self.spreadsheet
+		
 
 		
